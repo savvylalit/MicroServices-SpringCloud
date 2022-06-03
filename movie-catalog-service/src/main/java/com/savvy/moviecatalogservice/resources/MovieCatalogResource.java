f41ae444ff3c646b10d9e6cdda2aff6c86d8ef6a
savvylalit <savvylalit@gmail.com>
2022-06-03 13:02:05 +0530

    Cleaned up unwanted comments

--- movie-catalog-service/src/main/java/com/savvy/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/savvy/moviecatalogservice/resources/MovieCatalogResource.java
@@ -28,9 +28,6 @@ public class MovieCatalogResource {
 	@Autowired
 	private RestTemplate restTemplate;
 	
-//	@Autowired 
-//	private WebClient.Builder webClientBuilder;
-
 	@RequestMapping("/{userId}")
 	//@HystrixCommand(fallbackMethod = "getFallbackCatalog") // This was used for high level non-granular circuit breaking
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
@@ -38,39 +35,9 @@ public class MovieCatalogResource {
 		UserRating userRating = userRatingInfo.getUserRating(userId);
 
 		return userRating.getRatings().stream().map(rating -> movieInfo.getCatalogItem(rating)).collect(Collectors.toList());
-		// for each movie id, call movie info service and get details
 
-		// put them all together
-		
 	}
 
-	/*
-	 * @HystrixCommand(fallbackMethod = "getFallbackCatalogItem") private
-	 * CatalogItem getCatalogItem(Rating rating) { Movie movie =
-	 * restTemplate.getForObject("http://movie-info-service/movies/" +
-	 * rating.getMovieId(), Movie.class);
-	 * 
-	 * //WebClient way of calling service. This is asynchronous way. // Movie movie
-	 * = webClientBuilder.build() // .get() // .uri("http://localhost:8082/movies/"
-	 * + rating.getMovieId()) // .retrieve() // .bodyToMono(Movie.class) //
-	 * .block();
-	 * 
-	 * return new CatalogItem(movie.getName(), "desc", rating.getRating()); }
-	 * 
-	 * private CatalogItem getFallbackCatalogItem(Rating rating) { return new
-	 * CatalogItem("No Movie", "Movie unavailable", 0); }
-	 * 
-	 * @HystrixCommand(fallbackMethod = "getFallbackUserRating") private UserRating
-	 * getUserRating(String userId) { return
-	 * restTemplate.getForObject("http://movie-rating-service/ratingsdata/users/" +
-	 * userId, UserRating.class); }
-	 * 
-	 * private UserRating getFallbackUserRating(String userId) { UserRating
-	 * userRating = new UserRating(); userRating.setUserId(userId);
-	 * userRating.setRatings(Arrays.asList(new Rating("0", 0))); return userRating;
-	 * }
-	 */
-	
 	// This was used for high level non-granular circuit breaking
 	public List<CatalogItem> getFallbackCatalog(@PathVariable("userId") String userId) {
 		return Arrays.asList(new CatalogItem("No movie", "", 0));
